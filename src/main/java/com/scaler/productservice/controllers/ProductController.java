b81c7aab78cd173ef6a7cbcb54d586aa1dac73b3
Mohit7892 <mohitrag721@gmail.com>
2026-04-02 01:21:49 +0530

    added getAllProducts FS implementaion with response product type

--- src/main/java/com/scaler/productservice/controllers/ProductController.java
+++ src/main/java/com/scaler/productservice/controllers/ProductController.java
@@ -3,6 +3,9 @@ package com.scaler.productservice.controllers;
 import com.scaler.productservice.models.Product;
 import com.scaler.productservice.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +21,6 @@ public class ProductController {
     private ProductService productService;
 
     public ProductController(ProductService productService){
-
         this.productService = productService;
     }
 
@@ -31,13 +33,19 @@ public class ProductController {
     //Implement Get product Apis
     //1. Get a Product--localhost::8080/products/22
     @GetMapping("/{productId}")
-    public Product getProduct(@PathVariable("productId") long productId){
-        return productService.getProductById(productId);
+    public ResponseEntity<Product> getProduct(@PathVariable("productId") long productId){
+        if(productId <= 0){
+            throw new IllegalArgumentException("Invalid product id used");
+        }
+        Product product = productService.getProductById(productId);
+        if(product == null)
+            return new ResponseEntity<>(product, HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(product,HttpStatus.OK);
     }
 
     //2. Get all products --localhost::8080/products
     @GetMapping("")
     public List<Product> getAllProducts(){
-        return  null;
+        return productService.getAllProducts();
     }
 }
